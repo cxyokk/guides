7fb03ff5cc51a1e3d2cbc6a90ab0c734d81ac75a
Greg Lazarev <greg@thoughtbot.com>
2014-07-03 17:04:17 -0700

    Specify guideline for required keyword arguments

--- style/samples/ruby.rb
+++ style/samples/ruby.rb
@@ -13,6 +13,11 @@ class SomeClass
       each_method_lives_on_its_own_line
   end
 
+  def method_with_required_keyword_arguments(one:, two:)
+    @one = one
+    @two = two
+  end
+
   def method_with_multiline_block
     some_method_before_block(should_be_followed_by_a_newline)
 
